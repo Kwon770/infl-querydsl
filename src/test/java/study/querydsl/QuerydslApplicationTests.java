f11e6e6af2bb4c6530b5504a34d11d945cddc600
Kwon770 <sckwon770@gmail.com>
2023-07-13 09:39:29 +0900

    섹션1: 프로젝트 환경설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,16 @@
 package study.querydsl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
